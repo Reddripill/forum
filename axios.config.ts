5695db3111ced821c27fd1b728c821cfb3460877
Reddripill <reddripill@mail.ru>
2024-05-19 22:57:47 +0300

    change server url

--- axios.config.ts
+++ axios.config.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 export const axiosInstance = axios.create({
-   baseURL: "http://localhost:1337/api",
+   baseURL: "https://fortunate-badge-00cc300cbd.strapiapp.com/api",
 });
 
 axiosInstance.interceptors.request.use((config) => {
